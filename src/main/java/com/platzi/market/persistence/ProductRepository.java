fe7f0d25e445348f4576fb04130d100d382e5008
yesiddiaz15 <yesiddiaz15@gmail.com>
2021-10-19 15:03:39 -0500

    Create more Query Methods

--- src/main/java/com/platzi/market/persistence/ProductRepository.java
+++ src/main/java/com/platzi/market/persistence/ProductRepository.java
@@ -2,10 +2,12 @@ package com.platzi.market.persistence;
 
 import com.platzi.market.persistence.crud.ProductCrudRepository;
 import com.platzi.market.persistence.entity.Product;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 import java.util.Optional;
 
+@Repository
 public class ProductRepository {
     private ProductCrudRepository productCrudRepository;
 
@@ -13,11 +15,23 @@ public class ProductRepository {
         return (List<Product>) productCrudRepository.findAll();
     }
 
-    public List<Product> getByCategory(int idCategory){
+    public List<Product> getByCategory(int idCategory) {
         return productCrudRepository.findByIdCategoryOrderByNombreAsc(idCategory);
     }
 
-    public Optional<List<Product>> getScarce(int cantidad, boolean estado){
-        return productCrudRepository.findByStockQuantityLessThanAndEstado(cantidad, true);
+    public Optional<List<Product>> getScarce(int quantity) {
+        return productCrudRepository.findByStockQuantityLessThanAndEstado(quantity, true);
+    }
+
+    public Optional<Product> getProduct(int idProduct) {
+        return productCrudRepository.findById(idProduct);
+    }
+
+    public Product save(Product product) {
+        return productCrudRepository.save(product);
+    }
+
+    public void delete(int idProduct) {
+        productCrudRepository.deleteById(idProduct);
     }
 }
