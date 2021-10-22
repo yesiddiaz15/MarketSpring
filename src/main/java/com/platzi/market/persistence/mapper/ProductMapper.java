787f0dec46aa5ab8d34a9d264ff7f11ddf70cace
yesiddiaz15 <yesiddiaz15@gmail.com>
2021-10-22 09:42:20 -0500

    Change names english to spanish because the example is like this

---
+++ src/main/java/com/platzi/market/persistence/mapper/ProductMapper.java
@@ -0,0 +1,29 @@
+package com.platzi.market.persistence.mapper;
+
+import com.platzi.market.domain.Product;
+import com.platzi.market.persistence.entity.Producto;
+import org.mapstruct.InheritInverseConfiguration;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.Mappings;
+
+import java.util.List;
+
+@Mapper(componentModel = "spring", uses = {CategoryMapper.class})
+public interface ProductMapper {
+    @Mappings({
+            @Mapping(source = "idProducto", target = "productId"),
+            @Mapping(source = "nombre", target = "name"),
+            @Mapping(source = "idCategoria", target = "CategoryId"),
+            @Mapping(source = "precioVenta", target = "price"),
+            @Mapping(source = "cantidadStock", target = "stock"),
+            @Mapping(source = "estado", target = "active"),
+            @Mapping(source = "category", target = "domainCategory")
+    })
+    Product toProduct(Producto producto);
+    List<Product> toProducts(List<Producto> productos);
+
+    @InheritInverseConfiguration
+    @Mapping(target = "", ignore = true)
+    Producto toProducto(Product product);
+}
