fe2fdd5514b160134614d6728e4cce6462342de8
yesiddiaz15 <yesiddiaz15@gmail.com>
2021-10-25 08:50:03 -0500

    Create purchase repository and add consumes for API

--- src/main/java/com/platzi/market/persistence/entity/Cliente.java
+++ src/main/java/com/platzi/market/persistence/entity/Cliente.java
@@ -11,7 +11,7 @@ public class Cliente {
     private String id;
     private String nombre;
     private String apellidos;
-    private Integer celular;
+    private Long celular;
     private String direccion;
 
     @Column(name = "correo_electronico")
@@ -44,11 +44,11 @@ public class Cliente {
         this.apellidos = apellidos;
     }
 
-    public Integer getCelular() {
+    public Long getCelular() {
         return celular;
     }
 
-    public void setCelular(Integer celular) {
+    public void setCelular(Long celular) {
         this.celular = celular;
     }
 
